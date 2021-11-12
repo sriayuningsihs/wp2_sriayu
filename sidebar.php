9c4e83e7b7b0f993e1b23a29828bb9da7587d4a3
sriayuningsihs <91069714+sriayuningsihs@users.noreply.github.com>
2021-11-12 11:31:12 +0700

    tugas pertemuan 9

--- sidebar.php
+++ sidebar.php
@@ -1,27 +1,45 @@
-<aside class="main-sidebar">
-    <section class="sidebar">
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="<?= base_url() ?>/assets/dist/img/ava.jpg" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-                <p>PENGGUNA</p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
-
-        <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">MAIN NAVIGATION</li>
-            <li class="button">
-                <a href="<?= base_url() ?>sepatu/form">
-                    <i class="fa fa-plus-square"></i> <span>PEMESANAN</span>
-                </a>
-            </li>
-            <li class="button">
-                <a href="<?= base_url() ?>sepatu/index">
-                    <i class="fa fa-book"></i> <span>DAFTAR PEMESANAN</span>
-                </a>
-            </li>
-        </ul>
-    </section>
-</aside>
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebardark accordion" id="accordionSidebar">
+ <!-- Sidebar - Brand -->
+ <a class="sidebar-brand d-flex align-items-center 
+justify-content-center" href="index.html">
+ <div class="sidebar-brand-icon rotate-n-15">
+ <i class="fas fa-book"></i>
+ </div>
+ <div class="sidebar-brand-text mx-3">Pustaka 
+Booking</div>
+ </a>
+ <!-- Divider -->
+ <hr class="sidebar-divider">
+ 
+ <!-- Looping Menu-->
+ 
+ <!-- Heading -->
+ <div class="sidebar-heading">
+ Master Data
+ </div>
+ <!-- Nav Item - Dashboard -->
+ <li class="nav-item active">
+ <!-- Nav Item - Dashboard -->
+ <li class="nav-item">
+ <a class="nav-link pb-0" href="<?=
+base_url('buku'); ?>">
+ <i class="fa fa-fw fa book"></i>
+ <span>Data Buku</span></a>
+ </li>
+ <li class="nav-item">
+ <a class="nav-link pb-0" href="<?=
+base_url('user/anggota'); ?>">
+ <i class="fa fa-fw fa book"></i>
+ <span>Data Anggota</span></a>
+ </li>
+ </li>
+ <!-- Divider -->
+ <hr class="sidebar-divider mt-3">
+ <!-- Sidebar Toggler (Sidebar) -->
+ <div class="text-center d-none d-md-inline">
+ <button class="rounded-circle border-0"
+id="sidebarToggle"></button>
+</div>
+</ul>
+ <!-- End of Sidebar -- >
